ecc8bcb45d079b1f0f22ae8df0255fadac76c244
Lorien <LorienMCS@gmail.com>
2015-10-13 15:26:16 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var app = angular.module('portfolioApp', ['ngRoute', 'ngAnimate']);
+
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+
+  // $locationProvider.html5Mode(true);
+
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html',
+    })
+    .when('/about', {
+      templateUrl: 'partials/about.html',
+    })
+    .when('/projects', {
+      templateUrl: 'partials/projects.html',
+    })
+    .when('/experience', {
+      templateUrl: 'partials/experience.html',
+    })
+    .when('/contact', {
+      templateUrl: 'partials/contact.html',
+    }).otherwise({
+      redirectTo: '/'
+    });
+}]);
